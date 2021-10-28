a1455677d6a90dbb5a39c36bf5575a0a35e6deaf
shahinghasemi <shahinghasemi.me@gmail.com>
2021-10-28 11:54:27 +0330

    feat: deep neural network for OCC

--- FNN.py
+++ FNN.py
@@ -5,6 +5,29 @@ from torch.nn.modules.container import Sequential
 class FCNN(nn.Module):
     def __init__(self, inputShape, dropout):
         super(FCNN, self).__init__()
+        self.DNN = nn.Sequential(
+            nn.Linear(inputShape, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(32, 16),
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(16, 4),
+            nn.BatchNorm1d(4),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(4, 1),
+        )
         self.encoder = nn.Sequential(
             nn.Linear(inputShape, 128),
             nn.BatchNorm1d(128),
@@ -31,18 +54,14 @@ class FCNN(nn.Module):
         )
         
     def forward(self, x):
-        encoded = self.encoder(x)
-        reconstructed = self.decoder(encoded)
-        return reconstructed
-
+        return self.DNN(x)
 
 def trainFNN(dataDic, nEpochs, nBatchsize, dropout, lr):
     labels = torch.from_numpy(dataDic['y'])
     X = dataDic['X']
     model = FCNN(X.shape[1], dropout)
     # should add weighted loss
-    # BCELoss = nn.BCEWithLogitsLoss()
-    MSELoss = nn.MSELoss()
+    BCELoss = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     indices = np.arange(labels.shape[0])
@@ -53,8 +72,9 @@ def trainFNN(dataDic, nEpochs, nBatchsize, dropout, lr):
             batchIndex = indices[boundary:boundary + nBatchsize]
             batchLoss = 0
             XTrain = X[batchIndex].float()
-            reconstructed = model(XTrain)
-            dnnLoss = MSELoss(reconstructed, XTrain)
+            y_pred = model(XTrain)
+            y = labels[batchIndex].float()
+            dnnLoss = BCELoss(y_pred, y)
             batchLoss += dnnLoss
             optimizer.zero_grad()
             batchLoss.backward()
@@ -69,13 +89,8 @@ def trainFNN(dataDic, nEpochs, nBatchsize, dropout, lr):
 def testFNN(model, dataDic):
     # DNN Input
     model.eval()
-    MSELoss = nn.MSELoss(reduction='none')
     X = dataDic['X'].float()
-    reconstructed = model(X)
-
-    with torch.no_grad():
-        loss = MSELoss(reconstructed, X)
-
-    loss = torch.sum(loss, 1, True)
-    loss =  loss / torch.sum(loss, 0)
-    return 1 - loss
+    y_pred = model(X)
+    # BCEWithLogits append a sigmoid
+    sig = nn.Sigmoid()
+    return sig(y_pred)
--- results.md
+++ results.md
@@ -3,6 +3,8 @@
 | DNN OCC using 80% of positives and 50% of negatives | e, p, t, s | 5 | adam | matrix | concatenate | 64 | 50 | 0.4 | 0.001 | 21.4 % | 48.5% | 34.1% | 45.9% | 99.1% | 13.2% | 20.6%
 | DNN OCC using 80% of positives and 50% of negatives | e, p, t, s | 5 | adam | jaccard| concatenate | 64 | 50 | 0.4 | 0.001 | 21.4% | 48.5% | 34.1% | 45.9% | 99.1% | 13.2% | 20.6%
 | Reconstruction OCC using 80% of positives | e, p, t, s | 5 | adam | matrix |concatenate | 64 | 50 | 0.4 | 0.001 | 91.7% | 65.9% | 10.3% | 5.6% | 19% | 93.6% | 7.1%
+| Reconstruction OCC using 80% of positives | e, p, t, s | 5 | adam | jaccard | sum | 64 | 50 | 0.4 | 0.001 | 89.4% | 62.5% | 9.6% | 4.8% | 22.3% | 91.1% | 6.4%
+| Reconstruction OCC using 80% of positives | e, p, t, s | 5 | adam | jaccard | sum | 64 | 50 | 0.4 | 0.0001 | 83.5% | 57.1% | 6.6% | 3.9% | 22.7% | 85.1% | 3.9%
 | Reconstruction OCC using 80% of negatives | e, p, t, s | 5 | adam | matrix |concatenate| 64 | 50 | 0.4 | 0.001 | 39.7% | 52.1% | 56.3% | 40.2% | 99.2% | 1.3% | 39.3% |
 | SVM OCC using 80% of positives | e, p, t, s | 5 | - | matrix | concatenate |  - | - | - | - | 6.1% | - | 5% | - | 98.8% | 3.7% | 2.5% 
 | SVM OCC using 80% of positives | t | 5 | - | matrix | concatenate |  - | - | - | - | 10.1% | - | 5.2% | - | 98.1% | 7.8% | 2.6% 
@@ -12,4 +14,4 @@
 | SVM OCC using 80% of positives | e, p, t, s | 5 | - | jaccard | concatenate |  - | - | - | - | 4.8% | - | 4.9% | - | 98.8% | 2.4% | 2.5% 
 | SVM OCC using 80% of positives | t | 5 | - | jaccard | concatenate |  - | - | - | - | 5.6% | - | 5% | - | 98.6% | 3.1% | 2.5%
 
-[0.05002794 0.05601801 0.9862069  0.03196743 0.02566493 0. 0.        ]
+[0.03925031 0.57198619 0.06632089 0.83534846 0.22769481 0.85105971 0.03968687]
