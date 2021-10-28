fix: threshold calculation + it's now a cli parameter

main.py
@@ -19,6 +19,7 @@ parser.add_argument('--feature-list', help='the feature list to include', type=s
 parser.add_argument('--folds', help='number of folds for cross-validation',type=int,  default=5)
 parser.add_argument('--batchsize', help='batch-size for DNN',type=int, default=1000)
 parser.add_argument('--epoch', help='number of epochs to train in model',type=int, default=20)
+parser.add_argument('--thresholds', help='number of epochs to train in model',type=int, default=30)
 parser.add_argument('--dropout', help='dropout probability for DNN',type=float, default=0.3)
 parser.add_argument('--lr', help='learning rate for DNN',type=float, default=0.001)
 parser.add_argument('--agg', help='aggregation method for DNN input', type=str, default='concatenate')
@@ -34,6 +35,7 @@ BATCHSIZE = args.batchsize
 EMBEDDING = args.emb
 FOLDS = args.folds
 DROPOUT = args.dropout
+THRESHOLD_PERCENT = args.thresholds
 LEARNING_RATE= args.lr
 CLASSIFIER = args.clf
 AGGREGATION = args.agg
@@ -140,7 +142,7 @@ def crossValidation(drugDic, diseaseSim, totalInteractions, totalNonInteractions
         allDataDic['X'] = XTest
         if CLASSIFIER == 'MAFCN':
             y_pred_prob = testFNN(trainedModel, allDataDic).detach().numpy()
-            metric = np.array(calculateMetric(YTest, y_pred_prob))
+            metric = np.array(calculateMetric(YTest, y_pred_prob, THRESHOLD_PERCENT))
         if CLASSIFIER == 'OCC':
             pred_label = model.predict(allDataDic['X'])
             pred_label[pred_label == -1] = 0

metrics.py
@@ -2,11 +2,11 @@
 from sklearn.metrics import accuracy_score, f1_score, roc_curve, auc, recall_score, confusion_matrix, precision_score
 import numpy as np
 
-def calculateMetric(real_score, predict_score):
+def calculateMetric(real_score, predict_score, thresholdPercent):
     real_score = real_score.reshape((real_score.shape[0],))
     predict_score = predict_score.reshape((predict_score.shape[0],))
 
-    thresholds = thresholdCalculation(predict_score, 100)
+    thresholds = thresholdCalculation(predict_score, thresholdPercent)
     thresholds = np.mat(thresholds)
     thresholds_num = thresholds.shape[1]
     # ----------------------------- #
@@ -75,11 +75,11 @@ def thresholdCalculation(predict_score, percent):
     # thresholds_num = thresholds.shape[1]
     sorted_predict_score = np.array(sorted(list(set(np.array(predict_score).flatten()))))
     sorted_predict_score_num = len(sorted_predict_score)
-    print('number of unique prediction scores: ',sorted_predict_score_num)
+    print('number of unique prediction: ',sorted_predict_score_num, sorted_predict_score)
     numberOfThresholds = round(sorted_predict_score_num * percent/100)
     if numberOfThresholds == 0:
         return sorted_predict_score[0]
     steps = sorted_predict_score_num // numberOfThresholds
-    indexes = steps * np.arange(sorted_predict_score_num)
+    indexes = steps * np.arange(numberOfThresholds)
     thresholds = sorted_predict_score[indexes]
     return thresholds